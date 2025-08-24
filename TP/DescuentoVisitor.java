3430c197b886e423d8b4f5b66ce0e2fef89e668e
Camu <camila.castillo.manrique@gmail.com>
2025-08-24 15:42:30 -0300

    Descuento Visitor

--- TP/DescuentoVisitor.java
+++ TP/DescuentoVisitor.java
@@ -1,14 +1,20 @@
-public class DescuentoVisitor implements Visitor{
+public class DescuentoVisitor implements Visitor {
 
     public void visitExpress(Express unExpress) {
-       
+        if (unExpress.carga && unExpress.descarga) {
+            unExpress.precio -= (unExpress.precio % 10);
+        }
     }
 
     public void visitNacional(Nacional unNacional) {
-       
+        if (unNacional.canObjMax < 20) {
+            unNacional.precio -= (unNacional.precio % 5);
+        }
     }
 
     public void visitInternacional(Internacional unInternacional) {
-      
+        if (!unInternacional.aduana) {
+            unInternacional.precio -= (unInternacional.precio % 15);
+        }
     }
 }
